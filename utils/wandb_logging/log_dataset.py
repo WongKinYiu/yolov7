e2d64423189215d5f495bc49c3f79effacfeaab3
1chimaruGin <kyitharhein18@gmail.com>
2022-07-08 00:41:18 +0630

    Updated argparse --project default YOLOR to Yolov7

--- utils/wandb_logging/log_dataset.py
+++ utils/wandb_logging/log_dataset.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='data/coco.yaml', help='data.yaml path')
     parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
-    parser.add_argument('--project', type=str, default='YOLOR', help='name of W&B Project')
+    parser.add_argument('--project', type=str, default='Yolov7', help='name of W&B Project')
     opt = parser.parse_args()
     opt.resume = False  # Explicitly disallow resume check for dataset upload job
 
