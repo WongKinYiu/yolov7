13594cf6d42bc3a49ff226570aa77b6bf7615f7f
Mohammad Khoshbin <khoshbin.mohammad.mk@gmail.com>
2022-07-28 03:05:17 +0430

    add tensor.detach for yolo_pose bug (#333)

--- utils/plots.py
+++ utils/plots.py
@@ -439,8 +439,8 @@ def output_to_keypoint(output):
     for i, o in enumerate(output):
         kpts = o[:,6:]
         o = o[:,:6]
-        for index, (*box, conf, cls) in enumerate(o.cpu().numpy()):
-            targets.append([i, cls, *list(*xyxy2xywh(np.array(box)[None])), conf, *list(kpts.cpu().numpy()[index])])
+        for index, (*box, conf, cls) in enumerate(o.detach().cpu().numpy()):
+            targets.append([i, cls, *list(*xyxy2xywh(np.array(box)[None])), conf, *list(kpts.detach().cpu().numpy()[index])])
     return np.array(targets)
 
 
