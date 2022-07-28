0d882e553e2fd43f26d79f9f67a0f2cdae0e1a45
Dhia_Oussayed <75687069+Dhiaeddine-Oussayed@users.noreply.github.com>
2022-07-28 20:09:52 +0100

    Fixed a bug for Hyperparameters Evolution. (#344)
    * Update hyp.scratch.custom.yaml
    
    anchors parameter missing, evolution won't start without.
    
    * Update train.py
    
    Updated the hyperparameter evolution metadata variable to match the hyp.yaml files for the evolution to run successfully.
    
    * Update hyp.scratch.p5.yaml
    
    Added the anchors parameter, evolution don't start without it.
    
    * Update hyp.scratch.p6.yaml
    
    added the anchors parameter for the hyperparameter evolution.
    
    * Update hyp.scratch.tiny.yaml
    
    added the anchors parameter for the hyperparameters evolution
    
    * Update hyp.scratch.custom.yaml
    
    * Update hyp.scratch.tiny.yaml
    
    * Update hyp.scratch.p5.yaml
    
    * Update hyp.scratch.p6.yaml
    
    * Update train.py
    
    * Update train_aux.py

--- train.py
+++ train.py
@@ -638,8 +638,15 @@ if __name__ == '__main__':
                 'flipud': (1, 0.0, 1.0),  # image flip up-down (probability)
                 'fliplr': (0, 0.0, 1.0),  # image flip left-right (probability)
                 'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
-                'mixup': (1, 0.0, 1.0)}  # image mixup (probability)
-
+                'mixup': (1, 0.0, 1.0),   # image mixup (probability)
+                'copy_paste': (1, 0.0, 1.0),  # segment copy-paste (probability)
+                'paste_in': (1, 0.0, 1.0)}    # segment copy-paste (probability)
+        
+        with open(opt.hyp, errors='ignore') as f:
+            hyp = yaml.safe_load(f)  # load hyps dict
+            if 'anchors' not in hyp:  # anchors commented in hyp.yaml
+                hyp['anchors'] = 3
+                
         assert opt.local_rank == -1, 'DDP mode not implemented for --evolve'
         opt.notest, opt.nosave = True, True  # only test/save final epoch
         # ei = [isinstance(x, (int, float)) for x in hyp.values()]  # evolvable indices
