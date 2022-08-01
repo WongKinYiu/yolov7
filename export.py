f09f0212a89cdcbd51941dcad126f173f8a5154b
Ian <ignaz.k@live.de>
2022-08-01 14:35:21 +0200

    TorchScript-Lite export

--- export.py
+++ export.py
@@ -75,6 +75,17 @@ if __name__ == '__main__':
     except Exception as e:
         print('TorchScript export failure: %s' % e)
 
+    # TorchScript-Lite export
+    try:
+        print('\nStarting TorchScript-Lite export with torch %s...' % torch.__version__)
+        f = opt.weights.replace('.pt', '.torchscript.ptl')  # filename
+        ts = torch.jit.trace(model, img, strict=False)
+        ts = optimize_for_mobile(ts)
+        ts._save_for_lite_interpreter(f)
+        print('TorchScript-Lite export success, saved as %s' % f)
+    except Exception as e:
+        print('TorchScript-Lite export failure: %s' % e)
+
     # ONNX export
     try:
         import onnx
