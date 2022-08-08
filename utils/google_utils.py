c14ba0c297b3b5fc0374c917db798c88f9dd226c
spacewalk01 <selmee006@gmail.com>
2022-08-08 16:17:40 +0900

    fixed the problem of downloading models (#451)

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -26,7 +26,8 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
             assets = [x['name'] for x in response['assets']]  # release assets
             tag = response['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
-            assets = ['yolov7.pt']
+            assets = ['yolov7.pt', 'yolov7-tiny.pt', 'yolov7x.pt', 'yolov7-d6.pt', 'yolov7-e6.pt', 
+                      'yolov7-e6e.pt', 'yolov7-w6.pt']
             tag = subprocess.check_output('git tag', shell=True).decode().split()[-1]
 
         name = file.name
