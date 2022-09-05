cf6359ffd832271be186ff5c15f318cbd5b9101f
Doğan Keskin <98788987+DoganK01@users.noreply.github.com>
2022-09-05 17:04:46 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -126,6 +126,10 @@ def detect(save_img=False):
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                        cv2.putText(im0, f"Total Objects: {len(det)}", (0, 105), cv2.FONT_HERSHEY_TRIPLEX,
+                                    1, (0, 0, 0), 1)
+                        cv2.putText(im0, f"{s}", (0, 105), cv2.FONT_HERSHEY_TRIPLEX,
+                                    1, (255, 0, 0), 1)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
