9498fdf95de96c0d0ba66a631219931edc850e70
Serkan UYSAL <uysalserkan08@gmail.com>
2022-11-27 12:41:04 +0300

    Update xywh2xyxy
    Update wrong formula

--- utils/general.py
+++ utils/general.py
@@ -266,9 +266,9 @@ def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
-    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 1] = x[:, 1] + x[:, 3] / 2  # top left y
     y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
-    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    y[:, 3] = x[:, 1] - x[:, 3] / 2  # bottom right y
     return y
 
 
