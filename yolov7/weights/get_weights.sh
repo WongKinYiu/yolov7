7b0e96e1bba1b720eb9f0e54225f76359d167d91
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2022-11-28 11:23:47 +0800

    add weights for yolov7-tiny

--- yolov7/weights/get_weights.sh
+++ yolov7/weights/get_weights.sh
@@ -11,18 +11,20 @@ fi
 for arch in $@
 do
     case $arch in
+        yolov7-tiny)
+            gdown -O yolov7-tiny_state.pt "https://drive.google.com/uc?id=1GYzWblAU8IAiydMOl1ND8ppgnf85knXS&confirm=t";;
         yolov7)
-            gdown -O yolov7_state.pt https://drive.google.com/uc?id=1er9yZV2Yeep0JjC7JhB86-7iC6nWUy1F;;
+            gdown -O yolov7_state.pt "https://drive.google.com/uc?id=1er9yZV2Yeep0JjC7JhB86-7iC6nWUy1F&confirm=t";;
         yolov7x)
-            gdown -O yolov7x_state.pt https://drive.google.com/uc?id=1m_DGALC4XjpEW5bvt31wAFt33H-5jOto;;
+            gdown -O yolov7x_state.pt "https://drive.google.com/uc?id=1m_DGALC4XjpEW5bvt31wAFt33H-5jOto&confirm=t";;
         yolov7-w6)
-            gdown -O yolov7-w6_state.pt https://drive.google.com/uc?id=1RaRZ5uFJPnvDrVFxiOaYbpzGajej7i3Z;;
+            gdown -O yolov7-w6_state.pt "https://drive.google.com/uc?id=1RaRZ5uFJPnvDrVFxiOaYbpzGajej7i3Z&confirm=t";;
         yolov7-e6)
-            gdown -O yolov7-e6_state.pt https://drive.google.com/uc?id=1F_17FmnBwxm7ymGObdkD3JIZYv5fDiPU;;
+            gdown -O yolov7-e6_state.pt "https://drive.google.com/uc?id=1F_17FmnBwxm7ymGObdkD3JIZYv5fDiPU&confirm=t";;
         yolov7-d6)
-            gdown -O yolov7-d6_state.pt https://drive.google.com/uc?id=1Q2Uwr7S0bpKvnObbgpkAYvkNME_W3viW;;
+            gdown -O yolov7-d6_state.pt "https://drive.google.com/uc?id=1Q2Uwr7S0bpKvnObbgpkAYvkNME_W3viW&confirm=t";;
         yolov7-e6e)
-            gdown -O yolov7-e6e_state.pt https://drive.google.com/uc?id=1Dibo8dxQuReyfN9h_3UUgI5Wd_rADOk5;;
+            gdown -O yolov7-e6e_state.pt "https://drive.google.com/uc?id=1Dibo8dxQuReyfN9h_3UUgI5Wd_rADOk5&confirm=t";;
         *)
             echo $arch weights are not available;;
     esac
