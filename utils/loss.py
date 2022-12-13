f8e9fb7a39b6ef8af8efeb4de8a83542828151f6
mhwahdan <61120913+mhwahdan@users.noreply.github.com>
2022-12-13 15:22:02 +0200

    autochange the tensor types according to the target devices

--- utils/loss.py
+++ utils/loss.py
@@ -636,7 +636,7 @@ class ComputeLossOTA:
         return loss * bs, torch.cat((lbox, lobj, lcls, loss)).detach()
 
     def build_targets(self, p, targets, imgs):
-        
+        processorType = targets.device.type
         #indices, anch = self.find_positive(p, targets)
         indices, anch = self.find_3_positive(p, targets)
         #indices, anch = self.find_4_positive(p, targets)
@@ -682,7 +682,8 @@ class ComputeLossOTA:
                 all_gj.append(gj)
                 all_gi.append(gi)
                 all_anch.append(anch[i][idx])
-                from_which_layer.append((torch.ones(size=(len(b),)) * i).to('cuda'))
+                from_which_layer.append((torch.ones(size=(len(b),)) * i).to(processorType))
+                
                 fg_pred = pi[b, a, gj, gi]                
                 p_obj.append(fg_pred[:, 4:5])
                 p_cls.append(fg_pred[:, 5:])
@@ -753,9 +754,8 @@ class ComputeLossOTA:
                 matching_matrix[:, anchor_matching_gt > 1] *= 0.0
                 matching_matrix[cost_argmin, anchor_matching_gt > 1] = 1.0
             fg_mask_inboxes = matching_matrix.sum(0) > 0.0
-            fg_mask_inboxes = fg_mask_inboxes.to(torch.device('cuda'))
+            fg_mask_inboxes = fg_mask_inboxes.to(processorType)
             matched_gt_inds = matching_matrix[:, fg_mask_inboxes].argmax(0)
-            
         
             from_which_layer = from_which_layer[fg_mask_inboxes]
             all_b = all_b[fg_mask_inboxes]
