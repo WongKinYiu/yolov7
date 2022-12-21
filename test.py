255ba0dd0e7f70625c16cee6000a3b35a465c228
yuje <yuje.lee@nota.ai>
2022-12-21 06:40:58 +0000

    update for test

--- test.py
+++ test.py
@@ -9,14 +9,15 @@ import torch
 import yaml
 from tqdm import tqdm
 
-from models.experimental import attempt_load
+from models.experimental import attempt_load, Ensemble
+from models.yolo import Model
 from utils.datasets import create_dataloader
 from utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
     box_iou, non_max_suppression, scale_coords, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
 from utils.metrics import ap_per_class, ConfusionMatrix
 from utils.plots import plot_images, output_to_target, plot_study_txt
-from utils.torch_utils import select_device, time_synchronized, TracedModel
-
+from utils.torch_utils import select_device, time_synchronized, TracedModel, intersect_dicts
+# from utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first, is_parallel
 
 def test(data,
          weights=None,
@@ -289,14 +290,14 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
+    parser.add_argument('--weights', nargs='+', type=str, default=['/root/workspace/workspace_yolov7/yolov7/yolov7_training.pt'], help='model.pt path(s)')
+    parser.add_argument('--data', type=str, default='/root/workspace/workspace_yolov7/yolov7/data/coco.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.65, help='IOU threshold for NMS')
     parser.add_argument('--task', default='val', help='train, val, test, speed or study')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='7', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--verbose', action='store_true', help='report mAP by class')
@@ -309,6 +310,8 @@ if __name__ == '__main__':
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
     parser.add_argument('--v5-metric', action='store_true', help='assume maximum recall as 1.0 in AP calculation')
+    parser.add_argument('--cfg', type=str, default='/root/workspace/workspace_yolov7/yolov7/cfg/training/yolov7.yaml', help='model.yaml path')
+    parser.add_argument('--hyp', type=str, default='/root/workspace/workspace_yolov7/yolov7/data/hyp.scratch.p5.yaml', help='hyperparameters path')
     opt = parser.parse_args()
     opt.save_json |= opt.data.endswith('coco.yaml')
     opt.data = check_file(opt.data)  # check file
--- utils/google_utils.py
+++ utils/google_utils.py
@@ -28,7 +28,8 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
         except:  # fallback plan
             assets = ['yolov7.pt', 'yolov7-tiny.pt', 'yolov7x.pt', 'yolov7-d6.pt', 'yolov7-e6.pt', 
                       'yolov7-e6e.pt', 'yolov7-w6.pt']
-            tag = subprocess.check_output('git tag', shell=True).decode().split()[-1]
+            # tag = subprocess.check_output('git tag', shell=True).decode().split()[-1]
+            tag = 'v0.1'
 
         name = file.name
         if name in assets:
