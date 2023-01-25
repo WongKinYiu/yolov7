3b1673c4be568c62031c026e7ff216b4f31bda57
niko <koutsoubn8@students.rowan.edu>
2023-01-24 19:15:47 -0500

    setup for hds debug on lambda03

--- scripts/fine_tune.sh
+++ scripts/fine_tune.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Train
-nohup \
-python3 train.py \
---epochs 150 \
+
+python3 -m torch.distributed.launch --nproc_per_node 6 --master_port 9527 train.py \
+--epochs 100 \
 --workers 8 \
 --device 0,1,2,3,4,5 \
---batch-size 132 \
---data data/drone.yaml \
---img 480 480 \
+--batch-size 768 \
+--data data/hds.yaml \
+--img 480  480 \
 --cfg cfg/training/yolov7-tiny-drone.yaml \
 --entity "drone_detection" \
 --weights 'weights/yolov7-tiny.pt' \
---name yolov7-custom \
+--name NWD_hds_100k \
 --hyp data/hyp.drone.tiny.yaml \
 --multi-scale \
 --save_period 2 \
 --freeze 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 \
-| tee TEMP.out
+| tee NWD_hds_100k.out
+
+
+
