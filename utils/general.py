fe965813ef02219454d0795d53d4ee6524178d97
Caliber-X <shuvamdeyashi45@gmail.com>
2023-03-26 01:23:34 +0530

    updated increment path logic

--- utils/general.py
+++ utils/general.py
@@ -884,11 +884,11 @@ def increment_path(path, exist_ok=False, sep='', mkdir=False):
     if path.exists() and not exist_ok:
         path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
 
-        dirs = glob.glob(f"{path}{sep}*")   # similar paths
-        matches = [re.search(rf"{path.stem}{sep}(\d+)", d) for d in dirs]
-        i = [int(m.groups()[0]) for m in matches if m]  # indices
-        n = max(i) + 1 if i else 2  # increment number
-        path = Path(f"{path}{sep}{n}{suffix}")  # increment path
+        for n in range(2, 9999):
+            p = f'{path}{sep}{n}{suffix}'  # increment path
+            if not os.path.exists(p):  #
+                break
+        path = Path(p)
 
     if mkdir:
         path.mkdir(parents=True, exist_ok=True) # make directory
