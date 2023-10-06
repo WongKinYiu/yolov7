fb3bf8b8b01d0d935228e225cfd6b3761937f5d4
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2023-10-06 13:40:54 -0400

    Generate a lighter version of the dataset from a kwcoco file

--- scripts/translate_coffee_to_coco.py
+++ scripts/translate_coffee_to_coco.py
@@ -1,28 +1,87 @@
 import json
+import kwcoco
 import os
+import argparse
+
+import ubelt as ub
+
+from pathlib import Path
 
 
-with open("/data/PTG/cooking/annotations/coffee/berkeley/2022-11-05_whole/fine-tuning_new_obj_labels.mscoco.json") as f:
-    truth = json.load(f)
+def load_kwcoco(dset):
+    """Load a kwcoco dataset from file
+
+    :param dset: kwcoco object or a string pointing to a kwcoco file
+
+    :return: The loaded kwcoco object
+    :rtype: kwcoco.CocoDataset
+    """
+    # Load kwcoco file
+    if type(dset) == str:
+        dset_fn = dset
+        dset = kwcoco.CocoDataset(dset_fn)
+        dset.fpath = dset_fn
+        print(f"Loaded dset from file: {dset_fn}")
+    return dset
+
+def kwcoco_to_yolo(dset, output_dir, split):
+    truth = load_kwcoco(dset)
+
+    gid_to_aids = truth.index.gid_to_aids
+    gids = ub.argsort(ub.map_vals(len, gid_to_aids))
+    with open(f"{output_dir}/{split}.txt", "w") as split_file:
+        for gid in gids:
+            im = truth.imgs[gid]
+
+            im_id = im['id']
+            im_fname = im['file_name']
+            split_file.write(f"{im_fname}\n")
+
+            # Get all objects for this image
+            aids = gid_to_aids[gid]
+            anns = ub.dict_subset(truth.anns, aids)
+            im_objects = list(anns.values()) #[ann for ann in truth['annotations'] if ann['image_id'] == im_id]
+
+            im_fpath = f"{output_dir}/labels/{split}/" + os.path.splitext(os.path.basename(im_fname))[0] + ".txt"
+
+            with open(im_fpath, "w") as text_file:
+                for obj in im_objects:
+                    x,y,w,h = obj['bbox']
+                    x /= im["width"]
+                    w /= im["width"]
+                    y /= im["height"]
+                    h /= im["height"]
+
+                    text_file.write(f"{obj['category_id']} ")
+                    text_file.write(f"{x} {y} {x+w} {y} {x+w} {y+h} {x} {y+h}\n")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dset",
+        type=str,
+        default="train.mscoco.json",
+        help="Kwcoco dataset to convert",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="datasets",
+        help="Output folder",
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        default="train",
+        help="Dataset split",
+    )
+
+    args = parser.parse_args()
+
+    Path(f"{args.output_dir}/labels/{args.split}").mkdir(parents=True, exist_ok=True)
+
+    kwcoco_to_yolo(args.dset, args.output_dir, args.split)
 
 
-for im in truth['images']:
-    im_id = im['id']
-    im_fname = im['file_name']
-
-    # Get all objects for this image
-    im_objects = [ann for ann in truth['annotations'] if ann['image_id'] == im_id]
-
-    im_fpath = "/data/standard_datasets/coffee_coco/labels/train/" + os.path.splitext(os.path.basename(im_fname))[0] + ".txt"
-
-    with open(im_fpath, "w") as text_file:
-        for obj in im_objects:
-            x,y,w,h = obj['bbox']
-            x /= 1280.0
-            w /= 1280.0
-            y /= 720.0
-            h /= 720.0
-            text_file.write(f"{obj['category_id']} ")
-            text_file.write(f"{x} {y} {x+w} {y} {x+w} {y+h} {x} {y+h}\n")
-
-
+if __name__ == "__main__":
+    main()
