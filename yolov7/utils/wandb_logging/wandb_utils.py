a5753b8c4b6d6e75ae4079df30cab260260f6d60
abhilash <abhilash@ridecell.com>
2023-10-23 14:35:21 -0700

    move scripts back to yolov7 to help train.py script

--- yolov7/utils/wandb_logging/wandb_utils.py
+++ yolov7/utils/wandb_logging/wandb_utils.py
@@ -6,7 +6,7 @@ import torch
 import yaml
 from tqdm import tqdm
 
-sys.path.append(str(Path(__file__).parent.parent.parent))  # add utils/ to path
+#sys.path.append(str(Path(__file__).parent.parent.parent))  # add utils/ to path
 from utils.datasets import LoadImagesAndLabels
 from utils.datasets import img2label_paths
 from utils.general import colorstr, xywh2xyxy, check_dataset
