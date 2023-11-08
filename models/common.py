d8ca342c08d6f63e0ead25a3c6fe946e852ca869
b-hundreds <76562741+b-hundreds@users.noreply.github.com>
2023-11-08 10:10:52 +0700

    Update common.py

--- models/common.py
+++ models/common.py
@@ -16,7 +16,7 @@ from utils.datasets import letterbox
 from utils.general import non_max_suppression, make_divisible, scale_coords, increment_path, xyxy2xywh
 from utils.plots import color_list, plot_one_box
 from utils.torch_utils import time_synchronized
-from utils.activations import Mish
+from utils.activations import MemoryEfficientMish as Mish
 
 
 ##### basic ####
