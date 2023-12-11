fbc471eca7aa97934ce7720486d2fadb4c9cf81e
Jelmer Wilhelm <jelmerwilhelm@gmail.com>
2023-12-11 12:31:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -295,8 +295,11 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.65, help='IOU threshold for NMS')
+    parser.add_argument('--conf_thres', type=float, help='Confidence threshold for validation and mAP')
+    parser.add_argument('--iou_thres', type=float, help='NMS for validation and mAP')
+    parser.add_argument('--overlap', type=float, help='Minimum needed overlap (iou threshold) for validation and mAP')
+    parser.add_argument('--max_overlap', type=float,
+                        help='Maximum needed overlap (iou threshold) for validation and mAP; used for mAP=0.x-o.y')
     parser.add_argument('--task', default='val', help='train, val, test, speed or study')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
