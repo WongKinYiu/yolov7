b42289c1a716b8d8e7cc9333c243e2ea21780f74
Vladimir Valeyev <ValV@users.noreply.github.com>
2023-12-26 20:14:14 +0300

    Use find_namespace_packages to include whole yolov7 subtree

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='yolov7',
     version='0.1.0',
-    packages=['yolov7'],
+    packages=find_namespace_packages(include=['yolov7.*']),
     url='https://github.com/ValV/yolov7',
     license='GPLv3',
     author='ValV',
